af7b628045db7abd35f52ac9f9c2dc085d2f29e9
Frogz9000 <ihurtmyknee11@gmail.com>
2026-04-02 09:11:42 -0700

    added camera test

--- main.c
+++ main.c
@@ -7,12 +7,14 @@
 #include "i2c.h"
 #include "oled.h"
 #include "uart.h"
-
+#include "adc12.h"
+#include "camera.h"
 //macros, constants, enums
 enum test{
 	SERVO_TEST,
 	BLE_TEST,
-	OLED_TEST
+	OLED_TEST,
+	CAMERA_TEST
 };
 
 //main prototypes
@@ -20,8 +22,11 @@ void delay(int time_ms);
 void servo_test(void);
 void OLED_test(void);
 void bluetooth_test(void);
+void camera_test(void);
 void safe_startup_inits(void);
 void test_suite(enum test test_todo);
+
+
 
 //main functions
 void delay(int time_ms){
@@ -78,9 +83,20 @@ void bluetooth_test(){
 	}
 }
 
+
+void camera_test(){
+	while(1){
+		if(Camera_isDataReady()){	
+			OLED_DisplayCameraData(Camera_getData());
+		}
+	}
+}
+
 void safe_startup_inits(){
 	OLED_Init();
 	UART1_init();
+	ADC0_init();
+	Camera_init();
 	init_servo_motor(50, 0.075);
 }
 
@@ -88,11 +104,12 @@ void test_suite(enum test test_todo){
 	if(test_todo == SERVO_TEST){while(1){servo_test();}}
 	if(test_todo == BLE_TEST){while(1){bluetooth_test();}}
 	if(test_todo == OLED_TEST){OLED_test();}
+	if(test_todo == CAMERA_TEST){camera_test();}
 }
 
 int main(){	
 	safe_startup_inits();
-	test_suite(BLE_TEST);
+	test_suite(CAMERA_TEST);
 	//init_dc_motor0(10000,0.20);
 	//init_dc_motor1(10000,0.20)
 	return 0;
