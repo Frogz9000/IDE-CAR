f00e31fd8f70cb2b0d5db7d5a67aad745ce49dd0
Frogz9000 <ihurtmyknee11@gmail.com>
2026-04-22 11:50:23 -0700

    modified dc motor control

--- lib/DC_motor.c
+++ lib/DC_motor.c
@@ -7,11 +7,12 @@
 
 void init_dc_motor0(uint32_t frequency, double percentDutyCycle){
 	//reverse driver pin, starts with zero
-	TIMA0_PWM_freq_init(1, frequency, 0.0);
+	//TIMA0_PWM_freq_init(1, frequency, 0.0);
+	IOMUX->SECCFG.PINCM[IOMUX_PINCM29] |= (0x80 | 0x01); //set port to IO
+	GPIOA->DOESET31_0 |= 0x0C;
+	GPIOA->DOUTCLR31_0 |= 0x0C;  
 	//forward driver pin
 	TIMA0_PWM_freq_init(0, frequency, percentDutyCycle);
-	
-	
 }
 
 void motors_forward(double percentDutyCycle)
@@ -23,29 +24,22 @@ void motors_forward(double percentDutyCycle)
 
 void dc0_forward(double percentDutyCycle){
 	TIMA0_PWM_DutyCycle(0, percentDutyCycle);
-	TIMA0_PWM_DutyCycle(1, 0.0);
 }
 
-void dc0_backwards(double percentDutyCycle){
-	TIMA0_PWM_DutyCycle(0, 0.0);
-	TIMA0_PWM_DutyCycle(1, percentDutyCycle);
-}
 
 void init_dc_motor1(uint32_t frequency, double percentDutyCycle){
 	//reverse driver pin, starts with zero
-	TIMA0_PWM_freq_init(3, frequency, 0.0);
+	//TIMA0_PWM_freq_init(3, frequency, 0.0);
+	IOMUX->SECCFG.PINCM[IOMUX_PINCM30] |= (0x80 | 0x01); //set port to IO
+	GPIOA->DOESET31_0 |= 0x0D;
+	GPIOA->DOUTCLR31_0 |= 0x0D; 
 	//forward driver pin
 	TIMA0_PWM_freq_init(2, frequency, percentDutyCycle);
 }
 void dc1_forward(double percentDutyCycle){
 	TIMA0_PWM_DutyCycle(2, percentDutyCycle);
-	TIMA0_PWM_DutyCycle(3, 0.0);
 }
 
-void dc1_backwards(double percentDutyCycle){
-	TIMA0_PWM_DutyCycle(2, 0.0);
-	TIMA0_PWM_DutyCycle(3, percentDutyCycle);
-}
 
 void motor_enable(){
 	//enable GPIO A Peripheral
@@ -75,9 +69,7 @@ void motor_enable(){
 }
 
 void init_dc_motors(uint32_t frequency, double percentDutyCycle){
-	TIMA0_PWM_freq_init(1, frequency, 0.0);
-	TIMA0_PWM_freq_init(3, frequency, 0.0);
-	TIMA0_PWM_freq_init(0, frequency, percentDutyCycle);
-	TIMA0_PWM_freq_init(2, frequency, percentDutyCycle);
+	init_dc_motor0(frequency,percentDutyCycle);
+	init_dc_motor1(frequency,percentDutyCycle);
 	motor_enable();
 }
